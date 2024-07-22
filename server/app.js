fa20b12b4367469fd21ec2be39c1aaaddaab037a
skander braham <skander.braham1991@gmail.com>
2024-07-22 13:08:03 +0100

    starter pack

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const uploadImage = require("./uploadImage.js");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "99mb" }));
+
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.post("/uploadImage", (req, res) => {
+  uploadImage(req.body.image)
+    .then((url) => res.send(url))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.post("/uploadMultipleImages", (req, res) => {
+  uploadImage
+    .uploadMultipleImages(req.body.images)
+    .then((urls) => res.send(urls))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.listen(port, () => {
+  console.log(` listening at http://localhost:${port}`);
+});
---
+++ server/uploadImage.js
@@ -0,0 +1,54 @@
+var cloudinary = require("cloudinary").v2;
+
+const cloud_name = "ds6qgagac"
+const api_key = "534188181747942"
+const api_secret = "7i2enwnHPNNtBP1W4Hu56GJ3QI8"
+// const secure_url = "CLOUDINARY_URL=cloudinary://534188181747942:7i2enwnHPNNtBP1W4Hu56GJ3QI8@ds6qgagac"
+
+cloudinary.config({
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
+});
+
+const opts = {
+  overwrite: true,
+  invalidate: true,
+  resource_type: "auto",
+};
+
+const uploadImage = (image) => {
+  //imgage = > base64
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (error, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(error.message);
+      return reject({ message: error.message });
+    });
+  });
+};
+module.exports = (image) => {
+  //imgage = > base64
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(image, opts, (error, result) => {
+      if (result && result.secure_url) {
+        console.log(result.secure_url);
+        return resolve(result.secure_url);
+      }
+      console.log(error.message);
+      return reject({ message: error.message });
+    });
+  });
+};
+
+module.exports.uploadMultipleImages = (images) => {
+  return new Promise((resolve, reject) => {
+    const uploads = images.map((base) => uploadImage(base));
+    Promise.all(uploads)
+      .then((values) => resolve(values))
+      .catch((err) => reject(err));
+  });
+};
